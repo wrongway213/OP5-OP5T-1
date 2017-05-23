95df48a8cc5c52e303d0bc561733740b875549e6
Maya Erez <merez@codeaurora.org>
2017-05-23 22:26:25 +0300

    msm_11ad: define ops_keep_radio_on_during_sleep as static
    Define ops_keep_radio_on_during_sleep as static as it is used
    only in msm_11ad context.
    
    Change-Id: Id95579961ce5cdd3fbdf063578bd4d9144f8a725
    Signed-off-by: Maya Erez <merez@codeaurora.org>

--- drivers/platform/msm/msm_11ad/msm_11ad.c
+++ drivers/platform/msm/msm_11ad/msm_11ad.c
@@ -1482,7 +1482,7 @@ static int ops_notify(void *handle, enum wil_platform_event evt)
 	return rc;
 }
 
-bool ops_keep_radio_on_during_sleep(void *handle)
+static bool ops_keep_radio_on_during_sleep(void *handle)
 {
 	struct msm11ad_ctx *ctx = (struct msm11ad_ctx *)handle;
 
@@ -1573,4 +1573,5 @@ void msm_11ad_modexit(void)
 EXPORT_SYMBOL(msm_11ad_modexit);
 
 MODULE_LICENSE("GPL v2");
-MODULE_DESCRIPTION("Platform driver for qcom 11ad card")
+MODULE_DESCRIPTION("Platform driver for qcom 11ad card");
+
