4f3960e3e6723595250667eb5589111bd2df324d
apophis9283 <steaven.android@gmail.com>
2019-02-28 20:58:07 -0500

    Revert "fs: dynamic_fsync: introduce ver 3.0"
    This reverts commit 5913ffe0a4975a1fd927b084bd80b1e2f261fe78.

--- fs/dyn_sync_cntrl.c
+++ fs/dyn_sync_cntrl.c
@@ -2,7 +2,6 @@
  * Dynamic sync control driver V2
  * 
  * by andip71 (alias Lord Boeffla)
- * modified by xNombre (Andrzej Perczak)
  * 
  * All credits for original implemenation to faux123
  * 
@@ -11,48 +10,57 @@
 #include <linux/module.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
+#include <linux/mutex.h>
 #include <linux/notifier.h>
 #include <linux/reboot.h>
 #include <linux/writeback.h>
 #include <linux/dyn_sync_cntrl.h>
 #include <linux/state_notifier.h>
-#include <linux/fs.h>
-#include <linux/string.h>
+
+// fsync_mutex protects dyn_fsync_active during suspend / late resume transitions
+static DEFINE_MUTEX(fsync_mutex);
+
 
 // Declarations
+
 bool suspend_active __read_mostly = false;
-bool dyn_fsync_active = DYN_FSYNC_ACTIVE_DEFAULT;
+bool dyn_fsync_active __read_mostly = DYN_FSYNC_ACTIVE_DEFAULT;
 
 static struct notifier_block notifier;
-static struct delayed_work sync_work;
+
+extern void sync_filesystems(int wait);
+
 // Functions
 
 static ssize_t dyn_fsync_active_show(struct kobject *kobj,
 		struct kobj_attribute *attr, char *buf)
 {
-	return sprintf(buf, "%u\n", dyn_fsync_active);
+	return sprintf(buf, "%u\n", (dyn_fsync_active ? 1 : 0));
 }
 
 
 static ssize_t dyn_fsync_active_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	if(strtobool(buf, &dyn_fsync_active))
-		return -EINVAL;
+	unsigned int data;
 
-	if (dyn_fsync_active)
+	if(sscanf(buf, "%u\n", &data) == 1)
 	{
-		pr_info("%s: dynamic fsync enabled\n", __FUNCTION__);
-		if (state_register_client(&notifier))
+		if (data == 1) 
 		{
-			pr_err("%s: Failed to register lcd callback\n", __func__);
+			pr_info("%s: dynamic fsync enabled\n", __FUNCTION__);
+			dyn_fsync_active = true;
 		}
-	}
+		else if (data == 0) 
+		{
+			pr_info("%s: dynamic fsync disabled\n", __FUNCTION__);
+			dyn_fsync_active = false;
+		}
+		else
+			pr_info("%s: bad value: %u\n", __FUNCTION__, data);
+	} 
 	else
-	{
-		pr_info("%s: dynamic fsync disabled\n", __FUNCTION__);
-		state_unregister_client(&notifier);
-	}
+		pr_info("%s: unknown input!\n", __FUNCTION__);
 
 	return count;
 }
@@ -73,20 +81,21 @@ static ssize_t dyn_fsync_suspend_show(struct kobject *kobj,
 	return sprintf(buf, "suspend active: %u\n", suspend_active);
 }
 
-static void emergency_event(void)
+
+static void dyn_fsync_force_flush(void)
 {
-	if(dyn_fsync_active) {
-		cancel_delayed_work(&sync_work);
-		suspend_active = false;
-		emergency_sync();
-		pr_warn("dynamic fsync: force flush!\n");
-	}
+	sync_filesystems(0);
+	sync_filesystems(1);
 }
+
 
 static int dyn_fsync_panic_event(struct notifier_block *this,
 		unsigned long event, void *ptr)
 {
-	emergency_event();
+	suspend_active = false;
+	dyn_fsync_force_flush();
+	pr_warn("dynamic fsync: panic - force flush!\n");
+
 	return NOTIFY_DONE;
 }
 
@@ -94,24 +103,39 @@ static int dyn_fsync_panic_event(struct notifier_block *this,
 static int dyn_fsync_notify_sys(struct notifier_block *this, unsigned long code,
 				void *unused)
 {
-	if(code == SYS_DOWN || code == SYS_HALT)
-		emergency_event();
-
+	if (code == SYS_DOWN || code == SYS_HALT) 
+	{
+		suspend_active = false;
+		dyn_fsync_force_flush();
+		pr_warn("dynamic fsync: reboot - force flush!\n");
+	}
 	return NOTIFY_DONE;
 }
 
 static int state_notifier_callback(struct notifier_block *this,
-				unsigned long event, void *data)
+								unsigned long event, void *data)
 {
 	switch (event) 
 	{
 		case STATE_NOTIFIER_ACTIVE:
+			mutex_lock(&fsync_mutex);
+			
 			suspend_active = false;
-			schedule_delayed_work(&sync_work, msecs_to_jiffies(SYNC_WORK_DELAY_MSEC));
+
+			if (dyn_fsync_active) 
+			{
+				dyn_fsync_force_flush();
+			}
+			
+			mutex_unlock(&fsync_mutex);
 			break;
+			
 		case STATE_NOTIFIER_SUSPEND:
+			mutex_lock(&fsync_mutex);
 			suspend_active = true;
+			mutex_unlock(&fsync_mutex);
 			break;
+			
 		default:
 			break;
 	}
@@ -165,58 +189,67 @@ static int dyn_fsync_init(void)
 {
 	int sysfs_result;
 
+	register_reboot_notifier(&dyn_fsync_notifier);
+	
+	atomic_notifier_chain_register(&panic_notifier_list,
+		&dyn_fsync_panic_block);
+
 	dyn_fsync_kobj = kobject_create_and_add("dyn_fsync", kernel_kobj);
+
 	if (!dyn_fsync_kobj) 
 	{
 		pr_err("%s dyn_fsync_kobj create failed!\n", __FUNCTION__);
 		return -ENOMEM;
 	}
 
-	sysfs_result = sysfs_create_group(dyn_fsync_kobj, &dyn_fsync_active_attr_group);
+	sysfs_result = sysfs_create_group(dyn_fsync_kobj,
+			&dyn_fsync_active_attr_group);
+
 	if (sysfs_result) 
 	{
 		pr_err("%s dyn_fsync sysfs create failed!\n", __FUNCTION__);
-		goto err;
+		kobject_put(dyn_fsync_kobj);
 	}
 
 	notifier.notifier_call = state_notifier_callback;
-	if (dyn_fsync_active)
+	if (state_register_client(&notifier) != 0) 
 	{
-		pr_info("%s: dynamic fsync enabled\n", __FUNCTION__);
-		if (state_register_client(&notifier))
-		{
-			pr_err("%s: Failed to register lcd callback\n", __func__);
-		}
+		pr_err("%s: Failed to register lcd callback\n", __func__);
+
+		unregister_reboot_notifier(&dyn_fsync_notifier);
+
+		atomic_notifier_chain_unregister(&panic_notifier_list,
+			&dyn_fsync_panic_block);
+
+		if (dyn_fsync_kobj != NULL)
+			kobject_put(dyn_fsync_kobj);
+
+		return -EFAULT;
 	}
 
-	INIT_DELAYED_WORK(&sync_work, sync_filesystems);
-
-	register_reboot_notifier(&dyn_fsync_notifier);
-	atomic_notifier_chain_register(&panic_notifier_list,
-		&dyn_fsync_panic_block);
-
-	pr_info("%s dynamic fsync initialisation completed\n", __FUNCTION__);
+	pr_info("%s dynamic fsync initialisation complete\n", __FUNCTION__);
 
 	return sysfs_result;
-
-err:
-	kobject_put(dyn_fsync_kobj);
-	return -EFAULT;
 }
 
 
 static void dyn_fsync_exit(void)
 {
 	unregister_reboot_notifier(&dyn_fsync_notifier);
-	atomic_notifier_chain_unregister(&panic_notifier_list, &dyn_fsync_panic_block);
-	kobject_put(dyn_fsync_kobj);
-	state_unregister_client(&notifier);
 
+	atomic_notifier_chain_unregister(&panic_notifier_list,
+		&dyn_fsync_panic_block);
+
+	if (dyn_fsync_kobj != NULL)
+		kobject_put(dyn_fsync_kobj);
+	
+	state_unregister_client(&notifier);
+		
 	pr_info("%s dynamic fsync unregistration complete\n", __FUNCTION__);
 }
 
 module_init(dyn_fsync_init);
 module_exit(dyn_fsync_exit);
 
-MODULE_DESCRIPTION("dynamic fsync - automatic fs sync optimizer");
+MODULE_DESCRIPTION("dynamic fsync - automatic fs sync optimization for");
 MODULE_LICENSE("GPL v2");
